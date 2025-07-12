334a06346b174f3590a8f05be9930d9c13bbbad1
YAO1267 <S10268880@connect.np.edu.sg>
2025-07-12 23:09:07 +0800

    Add timestamp column to Comment table and medication schedule page's HTML form

--- src/controllers/medicationSchedule.js
+++ src/controllers/medicationSchedule.js
@@ -28,70 +28,68 @@ export async function getMediSchedule(req, res) {
  * @type {import("express").RequestHandler}
  */
 
-export async function updateSchedule(req,res) {
-    try{
-        const userId = parseInt(req.params.userId);
-        if (isNaN(userId)) {
-        res.status(400).json({ error: "Invalid user ID" });
-        return
+export async function updateSchedule(req, res) {
+  try {
+    const userId = parseInt(req.params.userId);
+    if (isNaN(userId)) {
+      res.status(400).json({ error: "Invalid user ID" });
+      return;
     }
-        const schedule = await model.updateSchedule(userId,req.body);
-        if (!schedule) {
-        res.status(404).json({ error: "Medication schedule not found" });
-        return;
+    const schedule = await model.updateSchedule(userId, req.body);
+    if (!schedule) {
+      res.status(404).json({ error: "Medication schedule not found" });
+      return;
     }
-     res.status(200).json(schedule);
-    }
-    catch (error) {
+    res.status(200).json(schedule);
+  } catch (error) {
     console.error("Controller error:", error);
     res.status(500).json({ error: "Error retrieving schedule" });
     return;
-    }
+  }
 }
-
 
 /**
  * create the schedule
  * @type {import("express").RequestHandler}
  */
-export async function createSchedule(req,res) {
-    try{
-        const userId = +req.params.userId;
+export async function createSchedule(req, res) {
+  try {
+    const userId = +req.params.userId;
     if (isNaN(userId)) {
-        res.status(400).json({ error: "Invalid User ID" });
-        return;
+      res.status(400).json({ error: "Invalid User ID" });
+      return;
     }
-        const newSchedule = await model.createSchedule(userId,req.body);
-        res.status(201).json(newSchedule);
-    } catch (error) {
-        console.error("Controller error:", error);
-        res.status(500).json({ error: "Error creating schedule" });
-        return;
-    }
+    const newSchedule = await model.createSchedule(userId, req.body);
+    res.status(201).json(newSchedule);
+  } catch (error) {
+    console.error("Controller error:", error);
+    res.status(500).json({ error: "Error creating schedule" });
+    return;
+  }
 }
 
 /**
  * delete the schedule
  * @type {import("express").RequestHandler}
  */
-export async function deleteSchedule(req,res) {
-    try{
-        const scheduleID = parseInt(req.params.scheduleId);
-        const userId = parseInt(req.params.userId);
-        if (isNaN(scheduleID)) {
-        res.status(400).json({ error: "Invalid schedule ID" })
-        return;
-        }
+export async function deleteSchedule(req, res) {
+  try {
+    const scheduleID = parseInt(req.params.scheduleId);
+    const userId = parseInt(req.params.userId);
+    if (isNaN(scheduleID)) {
+      res.status(400).json({ error: "Invalid schedule ID" });
+      return;
+    }
 
-        const schedule = await model.deleteSchedule(userId,scheduleID);
-        if (!schedule) {
-        res.status(404).json({ error: "Medication schedule not found" });
-        return;
-        }
-        res.json(schedule);
-    }catch (error) {
+    const schedule = await model.deleteSchedule(userId, scheduleID);
+    if (!schedule) {
+      res.status(404).json({ error: "Medication schedule not found" });
+      return;
+    }
+    res.json(schedule);
+  } catch (error) {
     console.error("Controller error:", error);
     res.status(500).json({ error: "Error retrieving schedule" });
     return;
   }
-};
+}
