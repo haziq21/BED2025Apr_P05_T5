6262a17ed9597a5608162c7dc7ef53d192076b59
YAO1267 <S10268880@connect.np.edu.sg>
2025-07-13 20:01:19 +0800

    Implement initial data population and Refine schedule form update logic

--- src/models/comment.js
+++ src/models/comment.js
@@ -5,12 +5,9 @@ import pool from "../db.js";
  * get the comment by userId
  * @param {number} userId
  */
-export async function getCommentById(userId){
-    try {
-      const result = await pool
-        .request()
-        .input("userId", userId)
-        .query(`
+export async function getCommentById(userId) {
+  try {
+    const result = await pool.request().input("userId", userId).query(`
             SELECT c.Comment,c.PostId,u.UserId,u.Name AS UserName 
             FROM Comment c 
             JOIN Users u ON c.UserId = u.UserId
@@ -22,16 +19,14 @@ export async function getCommentById(userId){
     console.error("Database error:", error);
     throw error;
   }
-};
+}
 
 /**
- * get all the comments on comment page 
+ * get all the comments on comment page
  */
-export async function getComment(){
-    try {
-      const result = await pool
-        .request()
-        .query(`
+export async function getComment() {
+  try {
+    const result = await pool.request().query(`
             SELECT c.Comment,c.PostId,u.UserId,u.Name AS UserName
             FROM Comment c 
             JOIN Users u ON c.UserId = u.UserId 
@@ -42,89 +37,87 @@ export async function getComment(){
     console.error("Database error:", error);
     throw error;
   }
-};
+}
 
 /**
  * update the comment by userId
- * @param {number} userId 
+ * @param {number} userId
  * @param {{PostId: number, Comment: string}} postData
  */
-export async function updateComment(userId,postData) {
-    try {
-        const request = pool.request()
-        .input("PostId", postData.PostId)
-        .input("userId",userId)
-        .input("Comment", postData.Comment)
-      
-        const result = await request.query(`
+export async function updateComment(userId, postData) {
+  try {
+    const request = pool
+      .request()
+      .input("PostId", postData.PostId)
+      .input("userId", userId)
+      .input("Comment", postData.Comment);
+
+    const result = await request.query(`
         UPDATE Comment
         SET 
             Comment = @Comment
         OUTPUT INSERTED.*
         WHERE PostId = @PostId AND UserId = @userId
     `);
-        if (result.recordset.length === 0) {
-        return { message: `No comment found with ID ${postData.PostId}.` };
-        }
+    if (result.recordset.length === 0) {
+      return { message: `No comment found with ID ${postData.PostId}.` };
+    }
 
-        return result.recordset[0];
+    return result.recordset[0];
   } catch (error) {
     console.error("Database error:", error);
     throw error;
   }
-};
-
-
-
-
+}
 
 /**
  * create comment by userId and PostId
  * @param {number} userId
  * @param {{PostId: number, Comment: string}} newComment
  */
-export async function createComment(userId,newComment){
-    try{
-        const request = pool.request()
-        .input("UserId", userId)
-        .input("Comment", newComment.Comment)
+export async function createComment(userId, newComment) {
+  try {
+    const request = pool
+      .request()
+      .input("UserId", userId)
+      .input("Comment", newComment.Comment);
 
-        const result = await request.query(`
+    const result = await request.query(`
         INSERT INTO Comment (UserId,Comment)
         OUTPUT INSERTED.*
         VALUES(@userId,@Comment)  
             `);
-        return result.recordset[0];
-    }catch (error) {
+    return result.recordset[0];
+  } catch (error) {
     console.error("Database error:", error);
     throw error;
   }
-};
-
+}
 
 /**
  * delete the comment by userId and PostId
- * @param {number} PostId 
+ * @param {number} PostId
  * @param {number} userId
  */
-export async function deleteComment(userId,PostId) {
-    try{
-        const request = pool.request()
-        .input("PostId",PostId)
-        .input("userId",userId)
+export async function deleteComment(userId, PostId) {
+  try {
+    const request = pool
+      .request()
+      .input("PostId", PostId)
+      .input("userId", userId);
 
-        const result = await request.query(`
+    const result = await request.query(`
         DELETE FROM Comment 
         OUTPUT DELETED.*
         WHERE PostId = @PostId AND UserId = @userId
             `);
 
-        if (result.recordset.length === 0) {
-        return { message: `No comment found with ID ${PostId}.` };
-        }
+    if (result.recordset.length === 0) {
+      return { message: `No comment found with ID ${PostId}.` };
+    }
 
-        return { message: `Comment with ID ${PostId} has been deleted.` };
-    }catch (error) {
+    return { message: `Comment with ID ${PostId} has been deleted.` };
+  } catch (error) {
     console.error("Database error:", error);
     throw error;
   }
--- src/public/global.js
+++ src/public/global.js
@@ -1,11 +1,13 @@
+/**
+ * indicate which page the user chooses from navigation bar
+ */
 document.addEventListener("DOMContentLoaded", () => {
   const navLinks = document.querySelectorAll(".topnav a");
 
-  navLinks.forEach(link => {
-  link.addEventListener("click", () => {
-    navLinks.forEach(l => l.classList.remove("active"));
-    link.classList.add("active"); 
+  navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      navLinks.forEach((l) => l.classList.remove("active"));
+      link.classList.add("active");
+    });
   });
 });
-});
-
